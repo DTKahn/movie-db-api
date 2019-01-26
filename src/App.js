6885d0d2d3a39f9a89d5dea92f80547ff042a5f3
Daniel <dtkahn@gmail.com>
2019-01-26 11:00:39 -0500

    intall axios and connect to movied db api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies : []
+    }
+  }
+
+  // https://api.themoviedb.org/3/movie/popular?api_key=972b4433f3e8f302aee3055dd209330c
+  
+  componentDidMount() {
+    axios({
+      url: 'https://api.themoviedb.org/3/movie/now_playing',
+      method: 'GET',
+      params: {
+        api_key: '972b4433f3e8f302aee3055dd209330c'
+      }
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Movies!</h1>
         </header>
       </div>
     );
